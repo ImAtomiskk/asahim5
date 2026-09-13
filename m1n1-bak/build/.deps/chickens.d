5f2292433a81e23816148f2cb6a700bfb6706293
ImAtomiskk <imcubical@proton.me>
2026-09-12 21:45:48 -0400

    Update m1n1 Base and Create Backup

---
+++ m1n1-bak/build/.deps/chickens.d
@@ -0,0 +1,17 @@
+build/chickens.o: src/chickens.c src/mcc.h src/types.h src/adt.h src/hv.h \
+ src/exception.h src/iodev.h src/utils.h src/cpu_regs.h \
+ src/arm_cpu_regs.h src/soc.h src/../config.h src/uartproxy.h \
+ src/memory.h
+src/mcc.h:
+src/types.h:
+src/adt.h:
+src/hv.h:
+src/exception.h:
+src/iodev.h:
+src/utils.h:
+src/cpu_regs.h:
+src/arm_cpu_regs.h:
+src/soc.h:
+src/../config.h:
+src/uartproxy.h:
+src/memory.h:
---
+++ m1n1-bak/build/aarch64-unknown-none-softfloat/release/deps/rust-cb98b49d7135530d.d
@@ -0,0 +1,18 @@
+/home/atomisk/Dokumenter/asahim5/m1n1/build/aarch64-unknown-none-softfloat/release/deps/rust-cb98b49d7135530d.d: src/lib.rs src/adt.rs src/dlmalloc.rs src/float.rs src/gpu/mod.rs src/gpu/hw/mod.rs src/gpu/hw/t600x.rs src/gpu/hw/t602x.rs src/gpu/hw/t8103.rs src/gpu/hw/t8112.rs src/gpu/initdata.rs src/gpu/raw.rs src/gpu/types.rs src/print.rs
+
+/home/atomisk/Dokumenter/asahim5/m1n1/build/aarch64-unknown-none-softfloat/release/deps/librust-cb98b49d7135530d.a: src/lib.rs src/adt.rs src/dlmalloc.rs src/float.rs src/gpu/mod.rs src/gpu/hw/mod.rs src/gpu/hw/t600x.rs src/gpu/hw/t602x.rs src/gpu/hw/t8103.rs src/gpu/hw/t8112.rs src/gpu/initdata.rs src/gpu/raw.rs src/gpu/types.rs src/print.rs
+
+src/lib.rs:
+src/adt.rs:
+src/dlmalloc.rs:
+src/float.rs:
+src/gpu/mod.rs:
+src/gpu/hw/mod.rs:
+src/gpu/hw/t600x.rs:
+src/gpu/hw/t602x.rs:
+src/gpu/hw/t8103.rs:
+src/gpu/hw/t8112.rs:
+src/gpu/initdata.rs:
+src/gpu/raw.rs:
+src/gpu/types.rs:
+src/print.rs:
